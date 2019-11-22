71cc10e3d88e03320e334cae759d11493932d891
Scott Willis <scottwillis@Scotts-MacBook-Pro.local>
2019-11-22 15:49:28 -0800

    working

--- test-api/schema-paginated/database.js
+++ test-api/schema-paginated/database.js
@@ -1,27 +1,28 @@
-import assert from 'assert'
-import path from 'path'
+import assert from 'assert';
+import path from 'path';
+require('dotenv').config()
 
 const dbType = process.env.DB
 
-const connection = process.env.NODE_ENV !== 'test' ?
-  pgUrl(process.env.PAGINATE ? 'test2' : 'test1') :
-  dbType === 'PG' ?
-    pgUrl(process.env.PAGINATE ? 'test2' : 'test1') :
-    dbType === 'MYSQL' ?
-      mysqlUrl(process.env.PAGINATE ? 'test2' : 'test1') :
-      dbType === 'ORACLE' ?
-        oracleUrl(process.env.PAGINATE ? 'test2' : 'test1') :
-        { filename: path.join(__dirname, '../data/db/test1-data.sl3') }
+const connection =  process.env.NODE_ENV !== 'test'
+    ? pgUrl(process.env.PAGINATE ? 'test2' : 'test1')
+    : dbType === 'PG'
+      ? pgUrl(process.env.PAGINATE ? 'test2' : 'test1')
+      : dbType === 'MYSQL'
+        ? mysqlUrl(process.env.PAGINATE ? 'test2' : 'test1')
+        : dbType === 'ORACLE'
+          ? oracleUrl(process.env.PAGINATE ? 'test2' : 'test1')
+          : { filename: path.join(__dirname, '../data/db/test1-data.sl3') }
 
-let client = 'sqlite3'
+let client = 'sqlite3';
 if (process.env.NODE_ENV !== 'test') {
-  client = 'pg'
+  client = 'pg';
 } else if (dbType === 'PG') {
-  client = 'pg'
+  client = 'pg';
 } else if (dbType === 'MYSQL') {
-  client = 'mysql'
+  client = 'mysql';
 } else if (dbType === 'ORACLE') {
-  client = 'oracledb'
+  client = 'oracledb';
 }
 
 console.log('connection to', { client, connection })
@@ -32,18 +33,26 @@ export default require('knex')({
 })
 
 function pgUrl(dbName) {
-  assert(process.env.PG_URL, 'Environment variable PG_URL must be defined, e.g. "postgres://user:pass@localhost/"')
+  assert(
+    process.env.PG_URL,
+    'Environment variable PG_URL must be defined, e.g. "postgres://user:pass@localhost/"'
+  )
   return process.env.PG_URL + dbName
 }
 
 function mysqlUrl(dbName) {
-  assert(process.env.MYSQL_URL, 'Environment variable MYSQL_URL must be defined, e.g. "mysql//user:pass@localhost/"')
+  assert(
+    process.env.MYSQL_URL,
+    'Environment variable MYSQL_URL must be defined, e.g. "mysql//user:pass@localhost/"'
+  )
   return process.env.MYSQL_URL + dbName
 }
 
 function oracleUrl(dbName) {
-  assert(process.env.MYSQL_URL, 'Environment variable ORACLE_URL must be defined, e.g. "pass@localhost:port/sid"')
+  assert(
+    process.env.MYSQL_URL,
+    'Environment variable ORACLE_URL must be defined, e.g. "pass@localhost:port/sid"'
+  )
   const [ password, connectString ] = process.env.ORACLE_URL.split('@')
   return { user: dbName, password, connectString, stmtCacheSize: 0 }
 }
-
